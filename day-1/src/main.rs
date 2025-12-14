7b9a31ddd1a87096a6af08ce0c8ece887ff45029
Nicolas Alberti <nicolas.alberti@MacBook-Air-di-Nicolas.local>
2025-12-14 13:56:54 +0100

    Done part 1.

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -1,19 +1,42 @@
 use std::{fs, panic};
 
-fn day_1(input: &str) {
-    let (direction_str, amount_str) = input.split_at(1);
-    let amount_result: Result<i32, std::num::ParseIntError> = amount_str.parse();
+fn day_1(input: &str) -> i32 {
+    let total_pos = 100;
+    let mut position = 50;
+    let mut zero_count = 0;
 
-    let amount = match amount_result {
-        Ok(num) => {
-            println!("Successfully parsed amount: {}", num);
-            num
+    for line in input.lines() {
+        if line.is_empty() {
+            continue; //Skip empty lines
         }
-        Err(e) => {
-            eprintln!("Error parsing amount '{}': {}", amount_str, e);
-            panic!("Could not parse input.");
+
+        let (direction_str, amount_str) = line.split_at(1);
+        let amount_result: Result<i32, std::num::ParseIntError> = amount_str.parse();
+
+        let amount = match amount_result {
+            Ok(num) => {
+                println!("Successfully parsed amount: {}", num);
+                num
+            }
+            Err(e) => {
+                eprintln!("Error parsing amount '{}': {}", amount_str, e);
+                panic!("Could not parse input.");
+            }
+        };
+
+        match direction_str {
+            "R" => position += amount,
+            "L" => position -= amount,
+            _ => continue,
         }
-    };
+
+        position = position.rem_euclid(total_pos);
+        if position == 0 {
+            zero_count += 1;
+        }
+    }
+
+    zero_count
 }
 
 fn main() {
